062e2d627d003e0b8dbcc5dcf3e4ae64495eefad
Dark Wood <anonymoushackerintheweb@gmail.com>
2024-05-31 11:25:55 +0545

    Added the login functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,6 @@
-import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-import generateTokenAndSetCookie from "../utils/generatetoken.js";
+import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
     try {
@@ -17,9 +17,11 @@ export const signup = async (req, res) => {
             return res.status(400).json({ error: "Username already exists" });
         }
 
-        // Hash password here
+        // HASH PASSWORD HERE
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
+
+        // https://avatar-placeholder.iran.liara.run/
 
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
         const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
@@ -33,9 +35,8 @@ export const signup = async (req, res) => {
         });
 
         if (newUser) {
-            // Generate JWT Token
+            // Generate JWT token here
             generateTokenAndSetCookie(newUser._id, res);
-
             await newUser.save();
 
             res.status(201).json({
@@ -45,10 +46,10 @@ export const signup = async (req, res) => {
                 profilePic: newUser.profilePic,
             });
         } else {
-            res.status(400).json({ error: "Invalid User Data" });
+            res.status(400).json({ error: "Invalid user data" });
         }
     } catch (error) {
-        console.log(`Error in signup controller: ${error.message}`);
+        console.log("Error in signup controller", error.message);
         res.status(500).json({ error: "Internal Server Error" });
     }
 };
@@ -64,7 +65,7 @@ export const login = async (req, res) => {
 
         if (!user || !isPasswordCorrect) {
             return res
-                .send(400)
+                .status(400)
                 .json({ error: "Invalid username or password" });
         }
 
@@ -77,17 +78,17 @@ export const login = async (req, res) => {
             profilePic: user.profilePic,
         });
     } catch (error) {
-        console.log(`Error in login controller: ${error.message}`);
+        console.log("Error in login controller", error.message);
         res.status(500).json({ error: "Internal Server Error" });
     }
 };
 
 export const logout = (req, res) => {
     try {
-        res.cookie("jwt", { maxAge: 0 });
+        res.cookie("jwt", "", { maxAge: 0 });
         res.status(200).json({ message: "Logged out successfully" });
     } catch (error) {
-        console.log(`Error in logout controller: ${error.message}`);
+        console.log("Error in logout controller", error.message);
         res.status(500).json({ error: "Internal Server Error" });
     }
 };
