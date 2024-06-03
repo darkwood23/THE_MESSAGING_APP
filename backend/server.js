2188fbeb98bfc62657efdc7bccf30bd164e370bf
Dark Wood <anonymoushackerintheweb@gmail.com>
2024-06-03 14:19:11 +0545

    Deployment setup

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
@@ -7,6 +8,8 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -17,6 +20,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/frontend", "dist", "index.html"))
+})
 server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server is listening at port ${PORT}`);
